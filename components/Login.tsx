7a188176632dba3f68f3c8e38bc99acfa72ddef1
tarun-patidar <trnpt97@gmail.com>
2024-08-04 03:20:43 +0530

    New Project Initial commit

---
+++ components/Login.tsx
@@ -0,0 +1,63 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Colors } from '@/constants/Colors';
+
+export default function Login() {
+  return (
+    <View>
+      <Image 
+        source={require("@/assets/images/login.jpg")}
+        style={{
+            width : "100%",
+            height : 550,
+        }}
+        />
+        <View style= {styles.container}>
+            <Text style =  {{      
+                fontSize: 28,
+                fontFamily: "outfit-bold",
+                textAlign : "center",
+                marginTop : 10
+            }}>
+                RUCHIKA SHRIRAM ARTWORKS
+            </Text>
+            <Text style = {{
+                fontSize : 18,
+                fontFamily : "outfit-medium",
+                textAlign : "center",
+                color : Colors.GREY,
+                marginTop : 25
+            }}>
+                Keep surfing and booking the art for your loved ones
+                to make their special days memorable "Happy Shopping"
+            </Text>
+            <TouchableOpacity style = {styles.button}>
+                <Text style={{
+                    color:Colors.WHITE,
+                    fontFamily : "outfit-medium",
+                    fontSize : 20,
+                    textAlign : "center",
+                }}>
+                    Get Started
+                </Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor : Colors.WHITE,
+        marginTop: -10,
+        borderTopRightRadius : 20,
+        borderTopLeftRadius : 20,
+        padding : 30,
+        height : "100%",
+    },
+    button : {
+        padding : 15,
+        backgroundColor : Colors.PRIMARY,
+        borderRadius : 99,
+        marginTop : "20%",
+    }
+});
